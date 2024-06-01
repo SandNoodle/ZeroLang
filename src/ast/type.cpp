86de5b57a8e35e2a6ed8dcc8a66e5e87c5d2f9fe
SandNoodle <cddgamer@gmail.com>
2024-06-02 01:24:44 +0200

    Add Type class.

---
+++ src/ast/type.cpp
@@ -0,0 +1,144 @@
+#include "ast/type.h"
+
+#include <unordered_map>
+#include <sstream>
+
+namespace soul
+{
+	//
+	// Array
+	//
+
+	array_type_t::array_type_t(const array_type_t& other) noexcept
+		: _contained_type(std::make_unique<type_t>(*other._contained_type.get())) {}
+
+	array_type_t::array_type_t(array_type_t&& other) noexcept
+		: _contained_type(std::make_unique<type_t>(*other._contained_type.get())) {}
+
+	array_type_t::array_type_t(type_t contained_type)
+		: _contained_type(std::make_unique<type_t>(std::move(contained_type))) {}
+
+	array_type_t& array_type_t::operator=(const array_type_t& other) noexcept
+	{
+		if (this == &other)
+			return *this;
+
+		this->_contained_type = std::make_unique<type_t>(*other._contained_type.get());
+		return *this;
+	}
+
+	array_type_t& array_type_t::operator=(array_type_t&& other) noexcept
+	{
+		if (this == &other)
+			return *this;
+
+		*this->_contained_type = std::move(*other._contained_type.get());
+		return *this;
+	}
+
+	bool array_type_t::operator==(const array_type_t& other) const noexcept
+	{
+		return *this->_contained_type == *other._contained_type;
+	}
+
+	auto array_type_t::operator<=>(const array_type_t& other) const noexcept
+	{
+		return std::tie(*this->_contained_type.get()) <=> std::tie(*other._contained_type.get());
+	}
+
+	array_type_t::operator std::string() const
+	{
+		std::stringstream ss;
+		ss << std::string(*this->_contained_type);
+		ss << "[]";
+		return ss.str();
+	}
+
+	const type_t& array_type_t::type() const noexcept
+	{
+		return *this->_contained_type;
+	}
+
+	type_t& array_type_t::type() noexcept
+	{
+		return *this->_contained_type;
+	}
+
+	//
+	// Struct
+	//
+
+	struct_type_t::struct_type_t(std::vector<type_t> types)
+		: _types(std::move(types)) {}
+
+	auto struct_type_t::operator<=>(const struct_type_t& other) const noexcept
+	{
+		return std::lexicographical_compare_three_way(this->_types.begin(),
+			                                          this->_types.end(),
+			                                          other._types.end(),
+			                                          other._types.end(),
+			                                          [](const auto& lhs, const auto& rhs) {
+			                                          	return lhs <=> rhs;
+			                                          });
+	}
+
+	struct_type_t::operator std::string() const
+	{
+		std::stringstream ss;
+		ss << "(";
+		for (size_t index = 0; index < this->_types.size(); ++index)
+		{
+			ss << std::string(this->_types[index]);
+			if (index != this->_types.size() - 1)
+				ss << ", ";
+		}
+		ss << ")";
+		return ss.str();
+	}
+
+	const std::vector<type_t>& struct_type_t::types() const noexcept
+	{
+		return this->_types;
+
+	}
+
+	std::vector<type_t>& struct_type_t::types() noexcept
+	{
+		return this->_types;
+	}
+
+	//
+	// Type
+	//
+
+	type_t::type_t(is_type_t auto type)
+		: _type(std::move(type)) {}
+
+	std::strong_ordering operator<=>(const type_t& lhs, const type_t& rhs) noexcept
+	{
+		return std::tie(lhs._type) <=> std::tie(rhs._type);
+	}
+
+	type_t::operator std::string() const
+	{
+		return std::visit([](const auto& arg) -> std::string {
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<T, builtin_type_t>) {
+				static const std::unordered_map<builtin_type_t, std::string> k_types = {
+					{ builtin_type_t::type_boolean,   "bool" },
+					{ builtin_type_t::type_character, "char" },
+					{ builtin_type_t::type_float,     "float" },
+					{ builtin_type_t::type_integer,   "integer" },
+					{ builtin_type_t::type_string,    "str" },
+					{ builtin_type_t::type_void,      "void" },
+				};
+				return k_types.at(arg);
+			} else if constexpr (std::is_constructible_v<T, std::string>) {
+				return std::string(arg);
+			} else {
+				return std::string("unknown type");
+			}
+		}, _type);
+	}
+
+}  // namespace soul
---
+++ src/ast/type.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+namespace soul
+{
+	/**
+	 * @brief Represents 'Primitive' type in the language.
+	 */
+	enum class builtin_type_t : uint8_t
+	{
+		type_boolean,
+		type_character,
+		type_float,
+		type_integer,
+		type_string,
+		type_void, // only for functions.
+	};
+	class array_type_t;
+	class struct_type_t;
+	class type_t;
+
+	std::strong_ordering operator<=>(const type_t& lhs, const type_t& rhs) noexcept;
+
+	template <typename T>
+	concept is_type_t = std::same_as<T, builtin_type_t> //
+	                 || std::same_as<T, array_type_t>   //
+	                 || std::same_as<T, struct_type_t>  //
+	                 || std::same_as<T, std::monostate> //
+	;
+
+	/**
+	 * @brief Represents 'Array' type in the language.
+	 * Holds an underlying type making up the Array.
+	 */
+	class array_type_t
+	{
+		private:
+			std::unique_ptr<type_t> _contained_type = std::make_unique<type_t>();
+
+		public:
+			array_type_t() noexcept = default;
+			array_type_t(const array_type_t&) noexcept;
+			array_type_t(array_type_t&&) noexcept;
+			explicit array_type_t(type_t contained_type);
+
+			array_type_t& operator=(const array_type_t&) noexcept;
+			array_type_t& operator=(array_type_t&&) noexcept;
+			bool operator==(const array_type_t& other) const noexcept;
+			auto operator<=>(const array_type_t& other) const noexcept;
+			explicit operator std::string() const;
+
+			/** @brief Returns the underlying type. */
+			const type_t& type() const noexcept;
+
+			/** @brief Returns the underlying type. */
+			type_t& type() noexcept;
+	};
+
+	/**
+	 * @brief Represents 'Struct' type in the language.
+	 * Holds multiple of underlying types making up the Struct.
+	 */
+	class struct_type_t
+	{
+		private:
+			std::vector<type_t> _types;
+
+		public:
+			struct_type_t() noexcept = default;
+			struct_type_t(const struct_type_t&) noexcept = default;
+			struct_type_t(struct_type_t&&) noexcept = default;
+			explicit struct_type_t(std::vector<type_t> types);
+
+			struct_type_t& operator=(const struct_type_t&) noexcept = default;
+			struct_type_t& operator=(struct_type_t&&) noexcept = default;
+			bool operator==(const struct_type_t& other) const noexcept = default;
+			auto operator<=>(const struct_type_t& other) const noexcept;
+			explicit operator std::string() const;
+
+			/** @brief Returns the underlying types. */
+			const std::vector<type_t>& types() const noexcept;
+
+			/** @brief Returns the underlying types. */
+			std::vector<type_t>& types() noexcept;
+	};
+
+	/**
+	 * @brief Represents a 'Type' of a variable, function, etc. in the language.
+	 */
+	class type_t
+	{
+		public:
+			using unknown_t = std::monostate;
+			using variant_t = std::variant<unknown_t, builtin_type_t, array_type_t, struct_type_t>;
+
+		private:
+			variant_t _type = unknown_t{};
+
+		public:
+			type_t() noexcept = default;
+			type_t(const type_t&) noexcept = default;
+			type_t(type_t&&) noexcept = default;
+			explicit type_t(is_type_t auto type);
+
+			type_t& operator=(const type_t&) noexcept = default;
+			type_t& operator=(type_t&&) noexcept = default;
+			bool operator==(const type_t&) const noexcept = default;
+			explicit operator std::string() const;
+
+			/**
+			 * @brief Verifies that std::variant holds the specified type.
+			 * @tparam T Type to verify.
+			 * @return Returns true if this is a given type, false otherwise.
+			 */
+			template <is_type_t T>
+			[[nodiscard]] bool is() const
+			{
+				return std::holds_alternative<T>(_type);
+			}
+
+			/**
+			 * @brief Returns given type held by the std::variant.
+			 * @important Verify the type with is() beforehand.
+			 * @tparam Type to return.
+			 */
+			template <is_type_t T>
+			[[nodiscard]] T& get()
+			{
+				return std::get<T>(_type);
+			}
+
+			/**
+			 * @brief Returns given type held by the std::variant.
+			 * @important Verify the type with is() beforehand.
+			 * @tparam Type to return.
+			 */
+			template <is_type_t T>
+			[[nodiscard]] const T& get() const
+			{
+				return std::get<T>(_type);
+			}
+
+			friend std::strong_ordering operator<=>(const type_t& lhs, const type_t& rhs) noexcept;
+	};
+
+	template <typename T>
+	T& operator<<(T& stream, const type_t& type)
+	{
+		stream << std::string(type);
+		return stream;
+	}
+}  // namespace soul
