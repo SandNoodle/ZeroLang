c990dc181e34a5ff9ab1b77f82c2e362470d8fa6
SandNoodle <cddgamer@gmail.com>
2025-10-05 22:39:17 +0200

    Introduce IR::BasicBlock class.

---
+++ src/ir/basic_block.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "core/types.h"
+#include "ir/instruction.h"
+
+#include <limits>
+#include <vector>
+
+namespace soul::ir
+{
+	class IRBuilder;
+
+	/**
+	 * @brief BasicBlock represents a straight-line code sequence with no branches
+	 * (with exception to basic block' inputs and outputs).
+	 */
+	struct BasicBlock
+	{
+		public:
+		using Label        = u32;
+		using Instructions = std::vector<std::unique_ptr<Instruction>>;
+		using BasicBlocks  = std::vector<BasicBlock*>;
+
+		static constexpr Label k_invalid_label = std::numeric_limits<Label>::max();
+
+		private:
+		Label        _label{ k_invalid_label };
+		BasicBlocks  _successors{};
+		Instructions _instructions{};
+
+		public:
+		constexpr BasicBlock() = default;
+		constexpr BasicBlock(Label label);
+		constexpr BasicBlock(const BasicBlock&)     = delete;
+		constexpr BasicBlock(BasicBlock&&) noexcept = default;
+		constexpr ~BasicBlock()                     = default;
+
+		BasicBlock&    operator=(const BasicBlock&)                        = delete;
+		BasicBlock&    operator=(BasicBlock&&) noexcept                    = default;
+		constexpr bool operator==(const BasicBlock& other) const noexcept  = default;
+		constexpr auto operator<=>(const BasicBlock& other) const noexcept = default;
+
+		[[nodiscard]] constexpr Label               label() const noexcept;
+		[[nodiscard]] constexpr const Instructions& instructions() const noexcept;
+		[[nodiscard]] constexpr const BasicBlocks&  successors() const noexcept;
+
+		friend IRBuilder;
+	};
+}  // namespace soul::ir
+#include "ir/basic_block.inl"
---
+++ src/ir/basic_block.inl
@@ -0,0 +1,10 @@
+namespace soul::ir
+{
+	constexpr BasicBlock::BasicBlock(Label label) : _label(label) {}
+
+	constexpr auto BasicBlock::label() const noexcept -> Label { return _label; }
+
+	constexpr auto BasicBlock::instructions() const noexcept -> const Instructions& { return _instructions; }
+
+	constexpr auto BasicBlock::successors() const noexcept -> const BasicBlocks& { return _successors; }
+}  // namespace soul::ir
